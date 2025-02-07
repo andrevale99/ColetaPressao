d4743ee9dae382bbfb6ba729c62be39621be71d0
andrevale99 <andrefvale@gmail.com>
2025-02-07 16:46:05 -0300

    Código para leitura do ADC

--- ColetaPressao/test/ATmega328p/main.c
+++ ColetaPressao/test/ATmega328p/main.c
@@ -1,14 +1,36 @@
+#include <stdint.h>
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/delay.h>
+
+uint16_t adc_value = 0;
 
 ISR(ADC_vect);
 
 int main(void)
 {
-    while(1)
-    {
+    ADMUX |= (1 << REFS0);
+    ADCSRA |= (1 << ADIE) |  (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+    ADCSRA |= (1 << ADEN);
+    ADCSRA |= (1 << ADSC);
 
+    sei();
+
+    while (1)
+    {
     }
 
     return 0;
+}
+
+ISR(ADC_vect)
+{
+    uint8_t status = SREG;
+
+    adc_value = ADC;
+
+    ADCSRA |= (1 << ADSC);
+
+    SREG = status;
 }
