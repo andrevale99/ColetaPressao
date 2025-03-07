f1b88926654a08f6d9d70af1f555ef91e1e5156c
andrevale99 <andrefvale@gmail.com>
2025-03-07 10:00:37 -0300

    Update

--- ColetaPressao/test/C/CheckFile.c
+++ ColetaPressao/test/C/CheckFile.c
@@ -10,32 +10,28 @@ char buffer[MAX_BUFFER];
 uint8_t cont = 0;
 FILE *arq;
 
-void check_file_exist(FILE *_arq, char *file_name, uint8_t len)
+void check_file_exist(FILE *_arq, char *file_name)
 {
     for (uint8_t sufixo = 0; sufixo < UINT8_MAX; ++sufixo)
     {
-        snprintf(file_name, len, "data_%d.txt", sufixo);
+        snprintf(file_name, PREFIX_FILE_NAME "_%d.txt", sufixo);
 
         _arq = fopen(file_name, "r");
-        if (_arq != NULL)
+        if (_arq == NULL)
         {
-            printf("Arquivo %s ENCONTRADO\n", file_name);
-            fclose(_arq);
-
-            sufixo++;
-
-            snprintf(file_name, len, "data_%d.txt", sufixo);
+            snprintf(file_name, PREFIX_FILE_NAME "_%d.txt", sufixo);
 
             return;
         }
+        fclose(_arq);
     }
 
-    snprintf(file_name, len, "data_%d.txt", 0);
+    snprintf(file_name, PREFIX_FILE_NAME "_%d.txt", 0);
 }
 
 int main()
 {
-    check_file_exist(arq, buffer, MAX_BUFFER);
+    check_file_exist(arq, buffer);
 
     printf("NOVO NOME: %s\n", buffer);
 
